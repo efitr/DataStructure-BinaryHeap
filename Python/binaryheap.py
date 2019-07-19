31aa074d8aae96b8a0e4da0cdca0479e6c152d80
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-18 20:35:56 -0700

    Starter Python3 BINARY HEAP basic class

--- Python/binaryheap.py
+++ Python/binaryheap.py
@@ -1,1 +1,41 @@
 # BinaryHeap
+class BinaryMinHeap(object):
+    """BinaryMinHeap: a partially ordered collection with efficient methods to
+    insert new items in partial order and to access and remove its minimum item.
+    Items are stored in a dynamic array that implicitly represents a complete
+    binary tree with root node at index 0 and last leaf node at index n-1."""
+
+    # Make initializer ( self , items=None )
+
+
+    # Helper function is empty ( self )
+
+
+    # Size function ( self )
+
+
+    # Function get minimun item ( self )
+
+
+    # Delete minimun item function ( self )
+
+
+    # Function replace minimun item ( self , item )
+
+
+    # Function bubble up ( self , index )
+
+
+    # Function bubble down ( self , index )
+
+
+    # Function last index ( self )
+
+
+    # Function parent index ( self , index )
+
+
+    # Function left child index ( self , index )
+
+
+    # Function right child index ( self , index )
