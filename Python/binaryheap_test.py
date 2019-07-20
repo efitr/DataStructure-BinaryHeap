678e369cf5017026f6cfb6335893fccca34e62a9
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-19 17:14:21 -0700

    basic test plan done, missing functions and everything

---
+++ Python/binaryheap_test.py
@@ -0,0 +1,55 @@
+# ----------------------------------------------------------
+# ----------------------------------------------------------
+# Test suite for a BinaryHeap DataStructure implemented on Python3.
+# ----------------------------------------------------------
+# ----------------------------------------------------------
+
+# Third Party Libraries
+# ----------------------------------------------------------
+import pytest
+
+# Own implementation of BinaryHeap
+# ----------------------------------------------------------
+from binaryheap import BinaryMinHeap, BinaryMaxHeap
+
+
+# Class BinaryMinHeap test suite
+# ----------------------------------------------------------
+
+# Test for __init__, elements should get in the binaryheap when initialized
+
+
+# Test is the binaryheap empy, if it is, you should get True, if it is not, False.
+
+
+# Test for the size of the binaryheap
+
+
+# Test get minimun item 
+
+
+# Test delete minimun item
+
+
+# Test replace the minimun item
+
+
+# Test bubble up, rearranging of items until the size property works
+
+
+# Test bubble down, rearrange the item until its at a correct level
+
+
+# Test last index of the python3 list
+
+
+# Test parent index
+
+
+# Test left child index
+
+
+# Test right child index
+
+
+
